382c906b973b881e382c5703456f551519fdb135
reswaraa <reswara.ad@gmail.com>
2024-12-07 18:46:53 +0800

    fix: update favicon icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   title: 'IT GTD Miniproject',
   description: 'IT GTD XXVII Miniproject Assignment',
   icons: {
-    icon: '/profile.png',
+    icon: '/favicon.png',
   },
 };
 
